d4041a9591954f4466a7f5512971352e38b2e871
Anthony Lim <ast.lim12@gmail.com>
2013-01-05 15:20:50 +0800

    Set up class collection

---
+++ classes/class.user.php
@@ -0,0 +1,26 @@
+<?php
+require_once('classes/class.database.php');
+
+class User
+{
+	protected $db;
+	public function __construct()
+	{
+		$this->db = new Database();
+	}
+	
+	public function get_user($username)
+	{
+		$result = $this->db->get_where('auth_users',array('username'=>$username));
+		if($result->num_rows() > 0)
+		{
+			return $result->row();
+		}
+		else{
+			return null;
+		}
+	}
+	
+}
+
+?>
